e9a9f5f5e8521f7931d89c76eb99bd0e2aa468bd
fred belotte <fredbelotte@outlook.com>
2020-07-15 15:31:33 -0500

    add hello csharp project

--- HelloCsharp/Program.cs
+++ HelloCsharp/Program.cs
@@ -2,11 +2,20 @@
 
 namespace HelloCsharp
 {
-    class Program
+  class Program
+  {
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.Write("enter firstname: ");
+      var firstName = Console.ReadLine(); //lazy, datatype is given based on content of Readline
+      Console.Write("enter lastname: ");
+      string lastName = Console.ReadLine(); //eager, datatype is explicit prior to content of Readline
+
+      Console.WriteLine(firstName + " " + lastName);
+      Console.WriteLine("{0} {1}", firstName, lastName);
+      Console.WriteLine($"{firstName} {lastName}");
+
+      Console.ReadLine();
     }
+  }
 }
